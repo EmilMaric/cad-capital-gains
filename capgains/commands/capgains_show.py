eaa62b328799c4483b446cc842d59f9e48984f40
Eddy Maric <eddy.maric98@gmail.com>
2020-05-17 18:04:25 -0700

    Changed GOOG to GOOGL and added test for both tickers

--- capgains/commands/capgains_show.py
+++ capgains/commands/capgains_show.py
@@ -4,12 +4,13 @@ columns = ['date', 'ticker', 'transaction_type',
            'action', 'quantity', 'price',
            'share_balance', 'capital_gain']
 
+pd.set_option('display.max_rows', None,
+              'display.max_columns', None,
+              'display.width', 1000)
+
 
 def show_capgains_table(collection, tickers=None):
     global columns
-    pd.set_option('display.max_rows', None,
-                  'display.max_columns', None,
-                  'display.width', 1000)
 
     query = build_query(tickers)
     results_count = collection.count_documents(query)
@@ -25,6 +26,7 @@ def show_capgains_table(collection, tickers=None):
 
 def build_query(tickers):
     query = dict()
+
     if tickers is not None and len(tickers) > 0:
         query['ticker'] = {"$in": tickers}
 
--- tests/test_capgains_show.py
+++ tests/test_capgains_show.py
@@ -47,7 +47,7 @@ def test_show_capgains_table_default(db, capfd):
     """)  # noqa: E501
 
 
-def test_show_capgains_table_ticker(db, capfd):
+def test_show_capgains_table_both_tickers(db, capfd):
     col = db.collection
 
     objects = [dict(id=0,
@@ -61,7 +61,44 @@ def test_show_capgains_table_ticker(db, capfd):
                     capital_gain=0),
 
                dict(id=1,
-                    ticker='GOOG',
+                    ticker='GOOGL',
+                    date=datetime.datetime(2018, 2, 20),
+                    transaction_type='RSU VEST',
+                    action='BUY',
+                    quantity=42,
+                    price=249.55,
+                    share_balance=63,
+                    capital_gain=0)]
+
+    for obj in objects:
+        col.insert_one(obj)
+
+    CapGainsShow.show_capgains_table(col, ['ANET', 'GOOGL'])
+
+    out, err = capfd.readouterr()
+
+    assert out == textwrap.dedent("""\
+            date ticker transaction_type action  quantity   price  share_balance  capital_gain
+    0 2018-02-15   ANET    ESPP PURCHASE    BUY        21  307.96             21             0
+    1 2018-02-20  GOOGL         RSU VEST    BUY        42  249.55             63             0
+    """)  # noqa: E501
+
+
+def test_show_capgains_table_one_ticker(db, capfd):
+    col = db.collection
+
+    objects = [dict(id=0,
+                    ticker='ANET',
+                    date=datetime.datetime(2018, 2, 15),
+                    transaction_type='ESPP PURCHASE',
+                    action='BUY',
+                    quantity=21,
+                    price=307.96,
+                    share_balance=21,
+                    capital_gain=0),
+
+               dict(id=1,
+                    ticker='GOOGL',
                     date=datetime.datetime(2018, 2, 20),
                     transaction_type='RSU VEST',
                     action='BUY',
@@ -97,7 +134,7 @@ def test_show_capgains_table_unknown_ticker(db, capfd):
                     capital_gain=0),
 
                dict(id=1,
-                    ticker='GOOG',
+                    ticker='GOOGL',
                     date=datetime.datetime(2018, 2, 20),
                     transaction_type='RSU VEST',
                     action='BUY',
@@ -132,7 +169,7 @@ def test_show_capgains_table_known_and_unknown_ticker(db, capfd):
                     capital_gain=0),
 
                dict(id=1,
-                    ticker='GOOG',
+                    ticker='GOOGL',
                     date=datetime.datetime(2018, 2, 20),
                     transaction_type='RSU VEST',
                     action='BUY',
@@ -144,20 +181,20 @@ def test_show_capgains_table_known_and_unknown_ticker(db, capfd):
     for obj in objects:
         col.insert_one(obj)
 
-    CapGainsShow.show_capgains_table(col, ['GOOG', 'FB'])
+    CapGainsShow.show_capgains_table(col, ['GOOGL', 'FB'])
 
     out, err = capfd.readouterr()
 
     assert out == textwrap.dedent("""\
             date ticker transaction_type action  quantity   price  share_balance  capital_gain
-    0 2018-02-20   GOOG         RSU VEST    BUY        42  249.55             63             0
+    0 2018-02-20  GOOGL         RSU VEST    BUY        42  249.55             63             0
     """)  # noqa: E501
 
 
 def test_show_capgains_table_empty_db(db, capfd):
     col = db.collection
 
-    CapGainsShow.show_capgains_table(col, ['ANET', 'GOOG', 'FB'])
+    CapGainsShow.show_capgains_table(col, ['ANET', 'GOOGL', 'FB'])
 
     out, err = capfd.readouterr()
 
