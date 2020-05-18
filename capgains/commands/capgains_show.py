e26ad7d13aaa25d4e417d8f571469fe989f93eaa
Eddy Maric <eddy.maric98@gmail.com>
2020-05-17 22:18:46 -0700

    Added csv support

--- capgains/commands/capgains_show.py
+++ capgains/commands/capgains_show.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import csv
+import os
+
 
 columns = ['date', 'ticker', 'transaction_type',
            'action', 'quantity', 'price',
@@ -9,7 +12,20 @@ pd.set_option('display.max_rows', None,
               'display.width', 1000)
 
 
-def show_capgains_table(data, tickers=None):
+def show_capgains_table(filepath, tickers=None):
+    if not os.path.isfile(filepath):
+        print("File does not exist")
+        return
+
+    # open csv file and load data into list of dicts (each row is a dict)
+    with open(filepath) as csv_file:
+        data = [{key: value for key, value in row.items()}
+                for row in csv.DictReader(csv_file)]
+
+        print_data(data, tickers)
+
+
+def print_data(data, tickers=None):
     global columns
 
     results = filter_data(data, tickers)
--- tests/test_capgains_show.py
+++ tests/test_capgains_show.py
@@ -1,125 +1,136 @@
 import capgains.commands.capgains_show as CapGainsShow
-import datetime
+from datetime import datetime as dt
 import textwrap
+import pytest
+import csv
 
 
-def test_show_capgains_table_default(capfd):
+@pytest.fixture()
+def empty_csv_file(tmpdir_factory, scope='module'):
+    filename = str(tmpdir_factory.mktemp("data").join("empty.csv"))
+    open(filename, 'a').close()
+    return filename
+
+
+@pytest.fixture()
+def csv_file_and_data(tmpdir_factory, scope='module'):
+    filename = str(tmpdir_factory.mktemp("data").join("data.csv"))
+
     objects = [dict(id=0,
                     ticker='ANET',
-                    date=datetime.datetime(2018, 2, 15),
+                    date=dt.strftime(dt(2018, 2, 15),
+                                     '%Y-%m-%d'),
                     transaction_type='ESPP PURCHASE',
                     action='BUY',
-                    quantity=21,
-                    price=307.96,
-                    share_balance=21,
-                    capital_gain=0),
+                    quantity='21',
+                    price='307.96',
+                    share_balance='21',
+                    capital_gain='0'),
 
                dict(id=1,
-                    ticker='ANET',
-                    date=datetime.datetime(2018, 2, 20),
+                    ticker='GOOGL',
+                    date=dt.strftime(dt(2018, 2, 20),
+                                     '%Y-%m-%d'),
                     transaction_type='RSU VEST',
                     action='BUY',
-                    quantity=42,
-                    price=249.55,
-                    share_balance=63,
-                    capital_gain=0)]
+                    quantity='42',
+                    price='249.55',
+                    share_balance='63',
+                    capital_gain='0')]
 
-    CapGainsShow.show_capgains_table(objects)
+    keys = objects[0].keys()
+
+    with open(filename, 'w') as input_file:
+        dict_writer = csv.DictWriter(input_file, keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(objects)
+
+    return filename, objects
+
+
+def test_show_capgains_table_default(csv_file_and_data, capfd):
+    filename, _ = csv_file_and_data
+
+    CapGainsShow.show_capgains_table(filename)
 
     out, err = capfd.readouterr()
 
     assert out == textwrap.dedent("""\
-            date ticker transaction_type action  quantity   price  share_balance  capital_gain
-    0 2018-02-15   ANET    ESPP PURCHASE    BUY        21  307.96             21             0
-    1 2018-02-20   ANET         RSU VEST    BUY        42  249.55             63             0
+             date ticker transaction_type action quantity   price share_balance capital_gain
+    0  2018-02-15   ANET    ESPP PURCHASE    BUY       21  307.96            21            0
+    1  2018-02-20  GOOGL         RSU VEST    BUY       42  249.55            63            0
     """)  # noqa: E501
 
 
-def test_show_capgains_table_both_tickers(capfd):
-    objects = [dict(id=0,
-                    ticker='ANET',
-                    date=datetime.datetime(2018, 2, 15),
-                    transaction_type='ESPP PURCHASE',
-                    action='BUY',
-                    quantity=21,
-                    price=307.96,
-                    share_balance=21,
-                    capital_gain=0),
+def test_show_capgains_table_empty_file(empty_csv_file, capfd):
+    filename = empty_csv_file
 
-               dict(id=1,
-                    ticker='GOOGL',
-                    date=datetime.datetime(2018, 2, 20),
-                    transaction_type='RSU VEST',
-                    action='BUY',
-                    quantity=42,
-                    price=249.55,
-                    share_balance=63,
-                    capital_gain=0)]
-
-    CapGainsShow.show_capgains_table(objects, ['ANET', 'GOOGL'])
+    CapGainsShow.show_capgains_table(filename)
 
     out, err = capfd.readouterr()
 
     assert out == textwrap.dedent("""\
-            date ticker transaction_type action  quantity   price  share_balance  capital_gain
-    0 2018-02-15   ANET    ESPP PURCHASE    BUY        21  307.96             21             0
-    1 2018-02-20  GOOGL         RSU VEST    BUY        42  249.55             63             0
-    """)  # noqa: E501
+    No results found
+    """)
 
 
-def test_show_capgains_table_one_ticker(capfd):
-    objects = [dict(id=0,
-                    ticker='ANET',
-                    date=datetime.datetime(2018, 2, 15),
-                    transaction_type='ESPP PURCHASE',
-                    action='BUY',
-                    quantity=21,
-                    price=307.96,
-                    share_balance=21,
-                    capital_gain=0),
+def test_show_capgains_table_file_not_found(capfd):
+    filename = 'filedoesnotexist.csv'
 
-               dict(id=1,
-                    ticker='GOOGL',
-                    date=datetime.datetime(2018, 2, 20),
-                    transaction_type='RSU VEST',
-                    action='BUY',
-                    quantity=42,
-                    price=249.55,
-                    share_balance=63,
-                    capital_gain=0)]
-
-    CapGainsShow.show_capgains_table(objects, ['ANET'])
+    CapGainsShow.show_capgains_table(filename)
 
     out, err = capfd.readouterr()
 
     assert out == textwrap.dedent("""\
-            date ticker transaction_type action  quantity   price  share_balance  capital_gain
-    0 2018-02-15   ANET    ESPP PURCHASE    BUY        21  307.96             21             0
+    File does not exist
+    """)
+
+
+def test_print_data_default(csv_file_and_data, capfd):
+    _, objects = csv_file_and_data
+
+    CapGainsShow.print_data(objects)
+
+    out, err = capfd.readouterr()
+
+    assert out == textwrap.dedent("""\
+             date ticker transaction_type action quantity   price share_balance capital_gain
+    0  2018-02-15   ANET    ESPP PURCHASE    BUY       21  307.96            21            0
+    1  2018-02-20  GOOGL         RSU VEST    BUY       42  249.55            63            0
     """)  # noqa: E501
 
 
-def test_show_capgains_table_unknown_ticker(capfd):
-    objects = [dict(id=0,
-                    ticker='ANET',
-                    date=datetime.datetime(2018, 2, 15),
-                    transaction_type='ESPP PURCHASE',
-                    action='BUY',
-                    quantity=21,
-                    price=307.96,
-                    share_balance=21,
-                    capital_gain=0),
+def test_print_data_both_tickers(csv_file_and_data, capfd):
+    _, objects = csv_file_and_data
 
-               dict(id=1,
-                    ticker='GOOGL',
-                    date=datetime.datetime(2018, 2, 20),
-                    transaction_type='RSU VEST',
-                    action='BUY',
-                    quantity=42,
-                    price=249.55,
-                    share_balance=63,
-                    capital_gain=0)]
+    CapGainsShow.print_data(objects, ['ANET', 'GOOGL'])
 
-    CapGainsShow.show_capgains_table(objects, ['FB'])
+    out, err = capfd.readouterr()
+
+    assert out == textwrap.dedent("""\
+             date ticker transaction_type action quantity   price share_balance capital_gain
+    0  2018-02-15   ANET    ESPP PURCHASE    BUY       21  307.96            21            0
+    1  2018-02-20  GOOGL         RSU VEST    BUY       42  249.55            63            0
+    """)  # noqa: E501
+
+
+def test_print_data_one_ticker(csv_file_and_data, capfd):
+    _, objects = csv_file_and_data
+
+    CapGainsShow.print_data(objects, ['ANET'])
+
+    out, err = capfd.readouterr()
+
+    assert out == textwrap.dedent("""\
+             date ticker transaction_type action quantity   price share_balance capital_gain
+    0  2018-02-15   ANET    ESPP PURCHASE    BUY       21  307.96            21            0
+    """)  # noqa: E501
+
+
+def test_print_data_unknown_ticker(csv_file_and_data, capfd):
+    _, objects = csv_file_and_data
+
+    CapGainsShow.print_data(objects, ['FB'])
 
     out, err = capfd.readouterr()
 
@@ -128,41 +139,21 @@ def test_show_capgains_table_unknown_ticker(capfd):
     """)  # noqa: E501
 
 
-def test_show_capgains_table_known_and_unknown_ticker(capfd):
-    objects = [dict(id=0,
-                    ticker='ANET',
-                    date=datetime.datetime(2018, 2, 15),
-                    transaction_type='ESPP PURCHASE',
-                    action='BUY',
-                    quantity=21,
-                    price=307.96,
-                    share_balance=21,
-                    capital_gain=0),
+def test_print_data_known_and_unknown_ticker(csv_file_and_data, capfd):
+    _, objects = csv_file_and_data
 
-               dict(id=1,
-                    ticker='GOOGL',
-                    date=datetime.datetime(2018, 2, 20),
-                    transaction_type='RSU VEST',
-                    action='BUY',
-                    quantity=42,
-                    price=249.55,
-                    share_balance=63,
-                    capital_gain=0)]
-
-    CapGainsShow.show_capgains_table(objects, ['GOOGL', 'FB'])
+    CapGainsShow.print_data(objects, ['GOOGL', 'FB'])
 
     out, err = capfd.readouterr()
 
     assert out == textwrap.dedent("""\
-            date ticker transaction_type action  quantity   price  share_balance  capital_gain
-    0 2018-02-20  GOOGL         RSU VEST    BUY        42  249.55             63             0
+             date ticker transaction_type action quantity   price share_balance capital_gain
+    0  2018-02-20  GOOGL         RSU VEST    BUY       42  249.55            63            0
     """)  # noqa: E501
 
 
-def test_show_capgains_table_empty_input(capfd):
-    objects = []
-
-    CapGainsShow.show_capgains_table(objects, ['ANET', 'GOOGL', 'FB'])
+def test_print_data_empty_input(capfd):
+    CapGainsShow.print_data([], ['ANET', 'GOOGL', 'FB'])
 
     out, err = capfd.readouterr()
 
