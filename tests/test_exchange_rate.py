8e674836105544aa99411412498af9ac61ff39c6
Eddy Maric <emaric98@gmail.com>
2020-09-01 12:51:24 -0700

    Added exchange rate test for CAD currency

--- tests/test_exchange_rate.py
+++ tests/test_exchange_rate.py
@@ -76,3 +76,9 @@ def test_exchange_rate_weekend_date(USD_exchange_rates_mock):
     er = ExchangeRate('USD', ExchangeRate.min_date, date.today())
     expected_rate = er.get_rate(friday)
     assert er.get_rate(sunday) == expected_rate
+
+
+def test_cad_to_cad_rate_is_1():
+    day = date(2020, 5, 22)
+    er = ExchangeRate('CAD', day, day)
+    assert er.get_rate(day) == 1
