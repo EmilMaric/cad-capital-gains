505e7d20abf02581f6dd771c75a0581a376d64f3
Andrew Cooke <acooke@nvidia.com>
2025-05-17 20:23:21 -0700

    Fix the tests.

--- tests/test_capgains_show.py
+++ tests/test_capgains_show.py
@@ -7,7 +7,7 @@ from capgains.transactions import Transactions
 
 def test_no_filter(transactions, capfd):
     """Testing capgains_show without any filters"""
-    CapGainsShow.capgains_show(transactions)
+    CapGainsShow.capgains_show(transactions, show_exchange_rate=False)
     out, _ = capfd.readouterr()
     assert out == """\
 +------------+---------------+----------+----------+-------+---------+--------------+------------+
@@ -23,7 +23,7 @@ def test_no_filter(transactions, capfd):
 
 def test_no_transactions(capfd):
     """Testing capgains_show without any transactions"""
-    CapGainsShow.capgains_show(Transactions([]))
+    CapGainsShow.capgains_show(Transactions([]), show_exchange_rate=False)
     out, _ = capfd.readouterr()
     assert out == """\
 No results found
@@ -32,7 +32,7 @@ No results found
 
 def test_ticker(transactions, capfd):
     """Testing capgains_show when providing a ticker filter"""
-    CapGainsShow.capgains_show(transactions, tickers=['ANET'])
+    CapGainsShow.capgains_show(transactions, show_exchange_rate=False, tickers=['ANET'])
     out, _ = capfd.readouterr()
     assert out == """\
 +------------+---------------+----------+----------+-------+---------+--------------+------------+
@@ -49,7 +49,7 @@ def test_unknown_ticker(transactions, capfd):
     """Testing capgains_show when providing a ticker not present in any
     transactions
     """
-    CapGainsShow.capgains_show(transactions, tickers=['FB'])
+    CapGainsShow.capgains_show(transactions, show_exchange_rate=False, tickers=['FB'])
     out, _ = capfd.readouterr()
     assert out == """\
 No results found
@@ -60,7 +60,7 @@ def test_known_ticker_and_unknown_ticker(transactions, capfd):
     """Testing capgains_show when providing a ticker not present in any
     transactions and a ticker that is present in a transaction
     """
-    CapGainsShow.capgains_show(transactions, ['GOOGL', 'FB'])
+    CapGainsShow.capgains_show(transactions, show_exchange_rate=False, tickers=['GOOGL', 'FB'])
     out, _ = capfd.readouterr()
     assert out == """\
 +------------+---------------+----------+----------+-------+---------+--------------+------------+
@@ -75,7 +75,7 @@ def test_multiple_tickers(transactions, capfd):
     """Testing capgains_show when providing multiple tickers present in any
     transactions
     """
-    CapGainsShow.capgains_show(transactions, ['ANET', 'GOOGL'])
+    CapGainsShow.capgains_show(transactions, show_exchange_rate=False, tickers=['ANET', 'GOOGL'])
     out, _ = capfd.readouterr()
     assert out == """\
 +------------+---------------+----------+----------+-------+---------+--------------+------------+
@@ -114,7 +114,7 @@ def test_partial_shares(capfd, requests_mock):
         partial_sell
     ])
 
-    CapGainsShow.capgains_show(transactions, ['ANET'])
+    CapGainsShow.capgains_show(transactions, show_exchange_rate=False, tickers=['ANET'])
     out, _ = capfd.readouterr()
     assert out == """\
 +------------+---------------+----------+----------+-------+---------+--------------+------------+
